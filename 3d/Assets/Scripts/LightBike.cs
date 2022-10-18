281649c3b7363b052954f51301607bb6d23c26e7
thebigh2nr03 <66183008+thebigh2nr03@users.noreply.github.com>
2022-10-18 15:34:22 -0400

    basic bike controller

---
+++ 3d/Assets/Scripts/LightBike.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBike : MonoBehaviour
+{
+    public WheelCollider F;
+    public WheelCollider R;
+    public Vector2 motorTorque;
+    public Vector2 brakeTorque;
+    public float steerAngle = 10f;
+    public GameObject cameraAxis;
+    public float cameraPositionLerpRate = 10f;
+    public float cameraRotationLerpRate = 7f;
+    public float steerLerpRate = 1f;
+    public float leanCoef = -10f;
+    [SerializeField]
+    private float currentSteer;
+    public float speedCompensation = 0.2f;
+    [SerializeField]
+    private float currentSpeed;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentSteer = Mathf.Lerp(currentSteer, Input.GetAxis("Horizontal"), steerLerpRate * Time.fixedDeltaTime);
+        
+        currentSpeed = rb.velocity.magnitude;
+        float veloComp = rb.velocity.magnitude > 35f ? Mathf.Clamp((40f - rb.velocity.magnitude) * 0.2f, 0f, 1f) : 1;
+        F.motorTorque = Mathf.Clamp(Input.GetAxis("Vertical"), 0f, 1f) * motorTorque.x * veloComp;
+        R.motorTorque = Mathf.Clamp(Input.GetAxis("Vertical"), 0f, 1f) * motorTorque.y * veloComp;
+
+        F.brakeTorque = Mathf.Clamp(-Input.GetAxis("Vertical"), 0f, 1f) * brakeTorque.x;
+        R.brakeTorque = Mathf.Clamp(-Input.GetAxis("Vertical"), 0f, 1f) * brakeTorque.y;
+
+        F.steerAngle = currentSteer * steerAngle;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, currentSteer * leanCoef);
+    }
+
+    void FixedUpdate(){
+        cameraAxis.transform.position = Vector3.Lerp(cameraAxis.transform.position, transform.position, cameraPositionLerpRate * Time.fixedDeltaTime);
+        cameraAxis.transform.rotation = Quaternion.Lerp(cameraAxis.transform.rotation, transform.rotation, cameraRotationLerpRate * Time.fixedDeltaTime);
+    }
+}
