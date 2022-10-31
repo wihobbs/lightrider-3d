4100833c3afa3609c39be531c3ad970592720746
lxaw <lawhalen@email.sc.edu>
2022-10-31 18:45:10 -0400

    basic pause implemented

---
+++ 3d/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(GameIsPaused){
+                Resume();
+            }
+            else{
+                Pause();
+            }
+        }
+    }
+
+    public void Resume(){
+        // hide menu
+        pauseMenuUI.SetActive(false);
+        // freeze time
+        Time.timeScale = 1f;
+        // set paused
+        GameIsPaused = false;
+    }
+    void Pause(){
+        // show menu
+        pauseMenuUI.SetActive(true);
+        // freeze time
+        Time.timeScale = 0f;
+        // set paused
+        GameIsPaused = true;
+    }
+    public void LoadMainMenu(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+    public void Quit(){
+        Application.Quit();
+    }
+}
